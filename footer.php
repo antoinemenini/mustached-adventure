917000892f6cf92b264fa48ffb4df3f5f1c524a8
Antoine Menini <antoinou2958@gmail.com>
2013-05-01 23:23:38 +0200

    Remove unnecessary comment from footer

--- footer.php
+++ footer.php
@@ -1,7 +1,5 @@
 
         </div>
         <?php wp_footer();
-        /* <script type="text/javascript" src="//use.typekit.net/ahi7okk.js"></script>
-        <script type="texit/javascript">try{Typekit.load();}catch(e){}</script> ?>*/
     </body>
 </html>
